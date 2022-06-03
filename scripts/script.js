5fca530fc00a012ef21112399b12a18a7032942e
Sichkar Yurii <si4kar2017@gmail.com>
2022-06-03 22:24:32 +0300

    Lesson_20

--- scripts/script.js
+++ scripts/script.js
@@ -1,69 +1,86 @@
-const GALLERY_URL = 'https://jsonplaceholder.typicode.com/albums';
-const PHOTOS_URL = 'https://jsonplaceholder.typicode.com/photos';
-const GALLERY_LIST_CLASS = 'list_elements';
-const ALBUM_CLASS = 'album-item';
+$(() => {
+    const GALLERY_URL = 'https://jsonplaceholder.typicode.com/albums';
+    const PHOTOS_URL = 'https://jsonplaceholder.typicode.com/photos';
+    const ALBUM_CLASS = 'album-item';
+    const PHOTOS_CLASS = 'photo';
 
-const galleryTemmplate = document.querySelector('.gallery_template').innerHTML;
-const albumTemplate = document.querySelector('.album_template').innerHTML;
-const gallleryEL = document.querySelector('.gallery_list');
-const photoEl = document.querySelector('.photo_block');
+    const galleryTemmplate = $('.gallery_template').html();
+    const albumTemplate = $('.album_template').html();
+    const $gallleryEL = $('.gallery_list');
+    const $photoEl = $('.photo_block');
+    const $insert_block = $('.insert_block');
 
-const galleryApi = new RequestHandler(GALLERY_URL);
-const albumApi = new RequestHandler(PHOTOS_URL);
+    const $galleryApi = new RequestHandler(GALLERY_URL);
+    const $albumApi = new RequestHandler(PHOTOS_URL);
+    const $preview = new ImagePreview();
 
-document.addEventListener('click', onClickAction);
+    $insert_block.on('click', onClickAction);
 
-init();
+    init();
 
-let galleryList = [];
-let photoList = [];
+    let galleryList = [];
+    let photoList = [];
 
-function init() {
-    fetchGalleryList();
-}
-
-function fetchGalleryList() {
-    return galleryApi.getList().then((data) => {
-        galleryList = data;
-        renderList();
-    })
-}
-
-function onClickAction(e) {
-    let id = getGalleryId(e.target);
-
-    if (e.target.classList.contains(ALBUM_CLASS)) {
-        fetchPhotosList(id);
-        e.preventDefault();
+    function init() {
+        fetchGalleryList();
     }
-}
 
-function getGalleryId(el) {
-    return el.dataset.id;
-}
+    function fetchGalleryList() {
+        return $galleryApi.getList().then((data) => {
+            galleryList = data;
+            renderList();
+        })
+    }
 
-function generateGalleryEl(list) {
-    return galleryTemmplate.replace('{{id}}', list.id)
-                            .replace('{{title}}', list.title);
-}
+    function onClickAction(e) {
+        let id = getGalleryId(e.target);
 
-function renderList() {
-    gallleryEL.innerHTML = galleryList.map(generateGalleryEl).join('\n');
-}
+        if (e.target.classList.contains(ALBUM_CLASS)) {
+            fetchPhotosList(id);
+            e.preventDefault();
+        }
 
-function fetchPhotosList(albumId) {
-    return albumApi.getList({ albumId }).then((data) => {
-        photoList = data;
-        renderPhotoList();
-    });
-}
+        if (e.target.classList.contains(PHOTOS_CLASS)) {
+            const url = getPhotoUrl(e.target);
 
-function generatePhotoEl(photos) {
-    return albumTemplate.replace('{{id}}', photos.id)
-                            .replace('{{source}}', photos.thumbnailUrl)
-                            .replace('{{title}}', photos.title);
-}
+            $preview.show(url);
 
-function renderPhotoList() {
-    photoEl.innerHTML = photoList.map(generatePhotoEl).join('\n');
-}
+            e.preventDefault();
+        }
+    }
+
+    function getGalleryId(el) {
+        return el.dataset.id;
+    }
+
+    function generateGalleryEl(list) {
+        return galleryTemmplate.replace('{{id}}', list.id)
+            .replace('{{title}}', list.title);
+    }
+
+    function renderList() {
+        $gallleryEL.html(galleryList.map(generateGalleryEl).join('\n'));
+    }
+
+    function fetchPhotosList(albumId) {
+        return $albumApi.getList({ albumId }).then((data) => {
+            photoList = data;
+            renderPhotoList();
+        });
+    }
+
+    function generate$photoEl(photos) {
+        return albumTemplate.replace('{{id}}', photos.id)
+            .replace('{{source}}', photos.thumbnailUrl)
+            .replace('{{title}}', photos.title)
+            .replace('{{url}}', photos.url);
+    }
+
+    function renderPhotoList() {
+        $photoEl.html(photoList.map(generate$photoEl).join('\n'));
+    }
+
+    function getPhotoUrl(el) {
+        return el.dataset.url;
+    }
+});
