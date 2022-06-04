72d9ac0ae70b3fbae20eeedd580d140b6c7d23de
Sichkar Yurii <si4kar2017@gmail.com>
2022-06-04 15:14:03 +0300

    Lesson_20

--- scripts/script.js
+++ scripts/script.js
@@ -17,17 +17,18 @@ $(() => {
 
     let galleryList = [];
     let photoList = [];
-    Fancybox.bind("[data-fancybox]", {
-    });
+
     function init() {
         fetchGalleryList();        // .then((galleryList) => fetchPhotosList(galleryList[0].id));
     }
+
     function fetchGalleryList() {
         return $galleryApi.getList().then((data) => {
             galleryList = data;
             renderList();
         })
     }
+
     function onClickAction(e) {
         let id = getGalleryId(e.target);
         if (e.target.classList.contains(ALBUM_CLASS)) {
@@ -36,7 +37,6 @@ $(() => {
         }
 
         if (e.target.classList.contains(PHOTOS_CLASS)) {
-            // const url = getPhotoUrl(e.target);
 
             Fancybox.bind("[data-fancybox]", {
 
@@ -45,33 +45,35 @@ $(() => {
             e.preventDefault();
         }
     }
+
     function getGalleryId(el) {
         return el.dataset.id;
     }
+
     function generateGalleryEl(list) {
         return galleryTemmplate.replace('{{id}}', list.id)
             .replace('{{title}}', list.title);
     }
+
     function renderList() {
         $gallleryEL.html(galleryList.map(generateGalleryEl).join('\n'));
     }
+
     function fetchPhotosList(albumId) {
         return $albumApi.getList({ albumId }).then((data) => {
             photoList = data;
             renderPhotoList();
         });
     }
+
     function generatePhotoEl(photos) {
         return albumTemplate.replace('{{id}}', photos.id)
-            .replace('{{source}}', photos.url)
+            .replace('{{source}}', photos.thumbnailUrl)
             .replace('{{title}}', photos.title)
             .replace('{{url}}', photos.url);
     }
+    
     function renderPhotoList() {
         $photoEl.html(photoList.map(generatePhotoEl).join('\n'));
     }
-
-    // function getPhotoUrl(el) {
-    //     return el.dataset.url;
-    // }
-    });
+});
